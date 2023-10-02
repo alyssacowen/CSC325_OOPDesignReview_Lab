d34f8f09d5c21eb3dbec4c08385d5d1b6d5db077
alyssa cowen <alyssarcowen@gmail.com>
2023-10-01 20:59:24 -0400

    Freshman class complete

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/Freshman.java
@@ -3,13 +3,13 @@ package com.mycompany.csc325_oop_designreview_lab;
 public abstract class Freshman extends Student{
     protected int credits;
 
-    //freshman constructor
+    //Freshman constructor
     public Freshman(String name, short age, int credits) {
         this.name = name;
         this.age = age;
         this.credits = credits;
     }
-    //freshman toString
+    //Freshman toString
     @Override
     public String toString() {
         return super.toString() + ", Credits: " + credits;
